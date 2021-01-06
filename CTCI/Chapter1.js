6780e1e5b9795113da4f9fa76a438c67bfe1e007
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2021-01-06 12:46:54 -0800

    complete CTCI 1.1

--- CTCI/Chapter1.js
+++ CTCI/Chapter1.js
@@ -1,1 +1,23 @@
 // Arrays and Strings
+
+// 1.1
+// Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+// cannot use additional data structures?
+
+const isUnique = (input) => {
+  if (typeof input !== 'string') {
+    return 'Invalid input';
+  }
+  let seenBefore = {};
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] in seenBefore) {
+      return false;
+    } else {
+      seenBefore[input[i]] = true;
+    }
+  }
+  return true;
+};
+
+console.log("isUnique('abc')", isUnique('abc'));
+console.log("isUnique('aaa')", isUnique('aaa'));
