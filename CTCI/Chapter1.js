3cf26bab02d9548bc823dafdbb846936bd5a16da
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2021-01-06 13:01:13 -0800

    add CTCI 1.2

--- CTCI/Chapter1.js
+++ CTCI/Chapter1.js
@@ -21,3 +21,29 @@ const isUnique = (input) => {
 
 console.log("isUnique('abc')", isUnique('abc'));
 console.log("isUnique('aaa')", isUnique('aaa'));
+
+// 1.2
+// Check Permutation: Given two strings,write a method to decide if one is a permutation of the
+// other.
+
+const checkPermutation = (string1, string2) => {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+
+  const sortedString1 = [...string1].sort().join('');
+  const sortedString2 = [...string2].sort().join('');
+
+  if (sortedString1 === sortedString2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(
+  "checkPermutation('puppy', 'kitten')",
+  checkPermutation('puppy', 'kitten')
+);
+console.log("checkPermutation('cat', 'rat')", checkPermutation('cat', 'rat'));
+console.log("checkPermutation('dog', 'god')", checkPermutation('dog', 'god'));
